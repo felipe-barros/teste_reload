fbf884163b90ba4d224669e7393d0a455be0ec61
Felipe Barros <bcfelipe@icloud.com>
2021-02-15 23:01:57 -0300

    Adicionar imagem principal da screen Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import Header from '../../components/Header';
 import style from './style';
 
-function Home(){
+function Home() {
+    const mainImgSrc = require('../../../assets/img/ilustration_lifestyle.png');
+
     return (
         <View style={style.container}>
             <Header />
+            <View style={style.imageContainer}>
+                <Image source={mainImgSrc} style={style.mainImage} />
+                <View style={style.textContainer}>
+                    <Text style={style.title}>Lifestyle</Text>
+                    <Text style={style.subTitle}>Get a holistic view of your activities to enhance your wellbeing and benefit from even more accurate recommendations.</Text>
+                </View>
+            </View>
         </View>
     )
 }
--- src/screens/Home/style.js
+++ src/screens/Home/style.js
@@ -1,8 +1,31 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#FFF'
+    },
+    imageContainer: {
+
+    },
+    mainImg: {
+        position: 'absolute'
+    },
+    textContainer: {
+        position: 'absolute',
+        padding: 20,
+        width: '50%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#FFF',
+        marginBottom: 15
+    },
+    subTitle: {
+        fontSize: 14,
+        color: '#FFF'
     }
 })
